1078598fbea6c96ca4f97c2d589f06f979ca51b4
Aliaksandr Dubadzelau <aliaksandr.dubadzelau@gmail.com>
2020-10-21 03:52:06 +0300

    refactoring commit

--- FirstModule/src/main/java/FirstCrazyClass.java
+++ FirstModule/src/main/java/FirstCrazyClass.java
@@ -5,25 +5,24 @@ import org.slf4j.LoggerFactory;
 public class FirstCrazyClass {
 
     private static final Logger logger = LoggerFactory.getLogger(FirstCrazyClass.class);
+    private static final int TEST_WRONG_AGE = -1;
+    private static final int TEST_AGE = 10;
+    private static final String TEST_NAME = "Alex";
 
     public static void main(String[] args) {
 
         logger.info("Create Worker worker.");
         Worker crazyClass = new Worker();
 
-        int wrongAge = -1;
-        int age = 10;
-        String name = "Alex";
-
-        logger.info("object.setAge({})", age);
-        crazyClass.setAge(age);
-        logger.info("object.setName({})", name);
-        crazyClass.setName(name);
+        logger.info("object.setAge({})", TEST_AGE);
+        crazyClass.setAge(TEST_AGE);
+        logger.info("object.setName({})", TEST_NAME);
+        crazyClass.setName(TEST_NAME);
         logger.info("object.printHello()");
         crazyClass.printHello();
         try {
-            logger.info("object.setAge({})", wrongAge);
-            crazyClass.setAge(wrongAge);
+            logger.info("object.setAge({})", TEST_WRONG_AGE);
+            crazyClass.setAge(TEST_WRONG_AGE);
         }catch (Exception e){
             logger.error("Exception: {}", e.toString());
         }
